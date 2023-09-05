4f2b2ed837c89a5dfe0c6461ee9daddd3a155a21
SSSanga <ksa901125@gmail.com>
2023-09-05 14:49:53 +0900

    SA_push: return 값 해결

--- main.py
+++ main.py
@@ -21,16 +21,17 @@ import pickle
 
 # /api_v1/mlmodelwithregression with dict params
 # method : post
+
 @app.post('/api_v1/mlmodelwithregression') 
 def mlmodelwithregression(data:dict) : 
     print('data with dict {}'.format(data))
     # data dict to 변수 활당
     ODI = float(data['ODI'])
     입원기간 = float(data['입원기간'])
-    통증기간 = float(data['통증기간'])
+    통증기간 = float(data['통증기간(월)'])
     수술시간 = float(data['수술시간'])
     수술기법 = float(data['수술기법'])
-    Seg_Angle = float(data['Seg_Angle'])
+    Seg_Angle = float(data['Seg Angle(raw)'])
 
     # pkl 파일 존재 확인 코드 필요
 
@@ -38,11 +39,13 @@ def mlmodelwithregression(data:dict) :
     # 학습 모델 불러와 예측
     with open('datasets/RecurrenceOfSurgery_model.pkl', 'rb') as regression_file:
         loaded_model = pickle.load(regression_file)
-        input_labels = [[ODI, 입원기간, 통증기간, 수술시간, 수술기법, Seg_Angle]] # 학습했던 설명변수 형식 맞게 적용
-        result_predict = loaded_model.predict(input_labels)
+        input_features = [[ODI, 입원기간, 통증기간, 수술시간, 수술기법, Seg_Angle]] # 학습했던 설명변수 형식 맞게 적용
+        result_predict = loaded_model.predict(input_features)
         print('Predict Location_of_herniation Result : {}'.format(result_predict))
         pass
 
-    # 예측값 리턴
-    result = {'Location_of_herniation':result_predict[0]}
-    return result
+        # # 예측값 리턴
+        # result = int({'Location_of_herniation':result_predict[0]})
+        # return result
+        result = int(result_predict[0])
+        return {'Location_of_herniation': result}
